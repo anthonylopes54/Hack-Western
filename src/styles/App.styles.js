fb4dff254fabfc5da21564590427e70c99150148
Eric Kim <erickim195@gmail.com>
2020-11-21 12:51:22 -0800

    app.styles added

--- src/styles/App.styles.js
+++ src/styles/App.styles.js
@@ -1,11 +1,9 @@
 import styled from 'styled-components';
 
 const AppStyles = styled.div`
-    @media only screen and (min-width: 500px) {
-        .header {
-            color: blue;
-            font-size: 100px;
-        }
+    .header {
+        color: blue;
+        font-size: 100px;
     }
 `;
 
