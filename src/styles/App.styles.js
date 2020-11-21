d8ecf312d4bf383e3161a8dd391557534afc971c
Eric Kim <erickim195@gmail.com>
2020-11-21 12:46:55 -0800

    first commit

---
+++ src/styles/App.styles.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+const AppStyles = styled.div`
+    @media only screen and (min-width: 500px) {
+        .header {
+            color: blue;
+            font-size: 100px;
+        }
+    }
+`;
+
+export default AppStyles;
