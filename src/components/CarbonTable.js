c1f9384c47bda715e4fc8bb6c87c0b42856f1116
Mikayla Louie <Mikayla.Louie@D2L.com>
2020-11-21 22:39:36 -0800

    changed carbon to co2

--- src/components/CarbonTable.js
+++ src/components/CarbonTable.js
@@ -12,10 +12,10 @@ import { useSelector } from "react-redux";
 const columns = [
   { id: "date", label: "Date", minWidth: 50 },
   { id: "item", label: "Item", minWidth: 50 },
-  { id: "carbonValue", label: "Carbon (kg)", minWidth: 80 },
+  { id: "carbonValue", label: "CO2 (kg)", minWidth: 80 },
   {
     id: "dollarValue",
-    label: "Carbon ($)",
+    label: "CO2 ($)",
     minWidth: 45,
   },
 ];
